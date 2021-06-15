70c55d50b331c2335e55c75c2767d47a1263b401
redfielldj747 <johnmredfield@gmail.com>
2021-06-14 17:51:34 -0700

    Fixed formatting in excel sheet

--- constants.py
+++ constants.py
@@ -6,5 +6,8 @@ MAX_COMMAND = 2
 MIN_FILES = 1
 MAX_FILES = 3
 
+MIN_PORT = 1
+MAX_PORT = 10000
+
 #default port for SSH
 SSH_DEFAULT_PORT = 22
--- main.py
+++ main.py
@@ -3,6 +3,12 @@
 import paramiko
 #csv is imported to write to csv files
 import csv
+import xlwt
+from xlwt import Workbook
+#from openpyxl import Workbook
+import pandas
+
+import xlsxwriter
 
 #importing getpass to obfuscate passwords
 import getpass
@@ -13,13 +19,15 @@ import helpers
 #constants contains well... constants
 import constants
 
+
+
 def main():
     #empty list to be filled later
     commands = []
     #empty list to be filled later
     files = []
 
-    print("You can choose to run apt list --installed, apt-mark showmanual or both and save them to separate csv files")
+    print("You can choose to run apt list --installed, apt-mark showmanual or both and save them as excel sheets")
     print("For more information on run apt list --installed and the information it provides enter 1 now. Press enter to skip.")
     choice = helpers.getValidNumber(constants.MIN_COMMAND,constants.MIN_COMMAND)
 
@@ -35,13 +43,15 @@ def main():
     host = helpers.validIPAddress()
     port = input("Enter the SSH port you wish to use -- default is port 22, press enter for default:")
 
+
+    port = helpers.getValidNumber(constants.MIN_PORT, constants.MAX_PORT)
     if port == "":
         port = constants.SSH_DEFAULT_PORT
 
     username = input("Enter the username you wish to SSH in to:")
     password = getpass.getpass(prompt="Please enter a password:")
 
-    print("Enter 1 for just apt-list, enter 2 for apt-mark, enter 3 for both")
+    print("Enter 1 for apt-list, enter 2 for apt-mark, enter 3 for both")
     command_choice = helpers.getValidNumber(constants.MIN_FILES, constants.MAX_FILES)
 
     if command_choice == 1:
@@ -75,6 +85,7 @@ def main():
         for i in apt_list_PackageName:
             if i != "Listing...\n":
                 i = i.strip('\n')
+                data2["Package Name:"].append(i)
                 apt_list_PackageName_stripped.append(i)
         ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command("apt list --installed | awk -F/ '{print $2}'")
         apt_list_Version = []
@@ -84,45 +95,48 @@ def main():
             j = j.strip('\n')
             data2["Version:"].append(j)
 
-        #writer = csv.DictWriter(apt_list, data2.keys())
-        writer = csv.writer(apt_list, data2.keys())
+        df = pandas.DataFrame.from_dict(data2, orient= 'index')
+        df = df.transpose()
 
-        writer.writerows(zip(["Package Name:"], ["Version:"]))
+        writer = pandas.ExcelWriter('AptList.xlsx', engine = 'xlsxwriter')
+        df.to_excel(writer, sheet_name = "Sheet", index = False)
 
-        writer.writerows(zip(apt_list_PackageName_stripped, apt_list_Version))
+        # for column in df:
+        #     column_width = max(df[column].astype(str).map(len).max(), len(column))
+        #     col_idx = df.columns.get_loc(column)
+        #     writer.sheets['Sheet'].set_column(col_idx, col_idx, column_width)
+
+        workbook = writer.book
+
+        worksheet = writer.sheets['Sheet']
+
+        # format = workbook.add_format({'text_wrap': True})
+
+        worksheet.set_column('A:B', 60)
+
+        writer.save()
+
+
+        # wb = Workbook()
+        #
+        # ws = wb.active
+        #
+        # ws_aptlist = wb.create_sheet("aptlist")
+        #
+        # for i in apt_list_PackageName_stripped:
+        #     ws_aptlist.append(i)
+        #
+        # ws.title = "Apt List Results"
+        #
+        # wb.save("aptlist.xlsx")
+
+        # writer = csv.writer(apt_list, data2.keys())
+        # writer.writerows(zip(["Package Name:"], ["Version:"]))
+        # writer.writerows(zip(apt_list_PackageName_stripped, apt_list_Version))
 
 
         #writer.writerows(zip(data2["Package Name":].values(), data2["Version:":].values()))
 
-
-
-
-    # for i in range(len(commands)):
-    #     ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(commands[i])
-    #
-    #     data = ssh_stdout.readlines()
-    #
-    #     if commands[i] == "apt list --installed | awk -F/ '{print $1}":
-    #         for i in data:
-    #             data2["Package Name:"].append(i)
-    #     if commands[i] == "apt list --installed | awk -F/ '{print $2}'":
-    #         for k in data:
-    #             if k != '\n':
-    #                 data2["Version:"].append(k)
-    #     fieldnames = data2.keys()
-    #
-    #     if command_choice == 1:
-    #         writer = csv.DictWriter(apt_list, fieldnames=fieldnames)
-    #
-    #     writer.writeheader()
-    #
-    #     for key,value in data2.items():
-    #         writer.writerow([key, value])
-    #     #     writer.writerow(data2["Package Name:"][l])
-    #
-    #     #writer.writerow(data2)
-    #     # for j in data:
-    #     #     writer.writerow([j])
 
 if __name__ == "__main__":
     main()
